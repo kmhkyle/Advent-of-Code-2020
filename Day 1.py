6d6182f0b893c7b27a5a35eec12c03643d46bf66
kmhkyle <43160736+kmhkyle@users.noreply.github.com>
2020-12-01 21:43:22 -0500

    Update Day 1.py

--- Day 1.py
+++ Day 1.py
@@ -6,11 +6,19 @@ for i in range(len(numbers)):
     firstNum = numbers[i]
     for j in range(iterator, len(numbers)):
         secondNum = numbers[j]
-        numSum = int(firstNum) + int(secondNum)
-        if numSum == 2020:
-            print(firstNum, secondNum)
-            print(int(firstNum) * int(secondNum))
-            break
+        # numSum = int(firstNum) + int(secondNum)
+        for k in range(iterator + 1, len(numbers)):
+            thirdNum = numbers[k]
+            numSum = int(firstNum) + int(secondNum) + int(thirdNum)
+            if numSum == 2020:
+                print(firstNum, secondNum, thirdNum)
+                print(int(firstNum) * int(secondNum) * int(thirdNum))
+                break
+        else:
+            continue
+        break
+        
+                
         
 
 
